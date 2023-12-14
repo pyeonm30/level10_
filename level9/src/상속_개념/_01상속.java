f4b6645b27e3c35be4a63208b1cd1dcf989f09e7
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2023-12-14 15:29:42 +0900

    Update _01상속.java
    값 추가

--- level9/src/상속_개념/_01상속.java
+++ level9/src/상속_개념/_01상속.java
@@ -3,8 +3,9 @@ package 상속_개념;
 public class _01상속 {
 
 	public static void main(String[] args) {
-
 		System.out.println("안녕~");
+		System.out.println("안녕~");
+		System.out.println("안녕1111111111");
 		System.out.println("안녕~");
 		
 	}
