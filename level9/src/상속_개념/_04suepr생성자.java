6693d246390967278d913ac0bed6b221210f9a5f
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2023-12-14 16:48:26 +0900

    Update _04suepr생성자.java

--- level9/src/상속_개념/_04suepr생성자.java
+++ level9/src/상속_개념/_04suepr생성자.java
@@ -1,10 +1,38 @@
 package 상속_개념;
 
+class Product{
+	String name;
+	int price;
+	public Product(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+	
+}
+
+class Fruit extends Product{
+//	Fruit(){
+//	super(); Product();
+//}
+	
+	String origin;
+	
+	public Fruit(String name, int price, String origin) {
+		super(name, price);
+		this.origin = origin;
+	}
+
+}
+
 public class _04suepr생성자 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Product meat = new Product("고기",10000);
+		Fruit banana = new Fruit("바나나", 2000, "필리핀");
+		
+		
 	}
 
 }
