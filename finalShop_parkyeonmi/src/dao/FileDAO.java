d39e8411f5b09a77d58b85d481454e99c3363656
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2023-12-27 12:05:40 +0900

    .
    .

--- finalShop_parkyeonmi/src/dao/FileDAO.java
+++ finalShop_parkyeonmi/src/dao/FileDAO.java
@@ -3,6 +3,7 @@ package dao;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,6 +17,8 @@ import java.nio.file.Paths;
 public class FileDAO {
 
 
+	private String txtPath = "src/files";
+	private Charset charSet = StandardCharsets.UTF_8;
 	enum FileName {
 		BOARD("board.txt"), MEMBER("member.txt"), ITEM("item.txt"), CART("cart.txt");
 		private String name;
@@ -37,6 +40,31 @@ public class FileDAO {
 		return instance;
 	}
 	
+	private String loadFile(FileName name) {
+		
+		Path path = Paths.get(txtPath , name.getName());
+		StringBuilder data = new StringBuilder();
+		
+		try( FileInputStream fis = new FileInputStream(path.toString());
+			 InputStreamReader ir = new InputStreamReader(fis, charSet);
+			 BufferedReader br = new BufferedReader(ir);
+				){
+			String line;
+			while((line= br.readLine()) != null) {
+				data.append(line);
+				data.append("\n");
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return data.toString().substring(0, data.toString().length()-1);
+	}
+	
+	
 	private void createFile(FileName name) {
 		Path path = Paths.get("src/files/" + name.getName());
 		try {
@@ -45,6 +73,8 @@ public class FileDAO {
 			//System.out.println("파일이 이미 있음");
 		}
 	}
+	
+	
 
 	private void init() {
 
---
+++ level9/src/_13유용한클래스/_03StringBliuder.java
@@ -0,0 +1,10 @@
+package _13유용한클래스;
+
+public class _03StringBliuder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
